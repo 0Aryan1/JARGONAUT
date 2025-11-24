9d0a060f998c16b8f870ee944167027aa74fa612
Aaryan <146386361+0Aryan1@users.noreply.github.com>
2025-11-25 02:45:31 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -178,8 +178,8 @@ function Home() {
                 handle="aryanagrawal"
                 status="Writing Amazing Content"
                 contactText="Get in Touch"
-                avatarUrl="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=800&q=80"
-                miniAvatarUrl="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=200&q=80"
+               avatarUrl="/dev.jpeg"
+                miniAvatarUrl="/dev.jpeg"
                 showUserInfo={true}
                 enableTilt={true}
                 enableMobileTilt={false}
