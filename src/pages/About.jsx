39e6f2b2ba6508b02e0c454d2fe0d54b765bc599
Aaryan <146386361+0Aryan1@users.noreply.github.com>
2025-12-29 23:30:07 +0530

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -47,9 +47,9 @@ function About() {
               >
                 <span className="text-white text-xl">←</span>
                 <img 
-                  src="/logo.png" 
+                  src="/wlogo.png" 
                   alt="The Jargonaut Logo" 
-                  className="h-14 md:h-20 lg:h-24 w-auto object-contain"
+                  className="h-12 md:h-14 lg:h-16 w-auto object-contain"
                 />
               </button>
               
