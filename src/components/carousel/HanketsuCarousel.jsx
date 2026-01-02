250ab47130dc33b4aceedf51673150cb51067f8f
Aaryan <146386361+0Aryan1@users.noreply.github.com>
2026-01-02 20:58:08 +0530

    Update HanketsuCarousel.jsx

--- src/components/carousel/HanketsuCarousel.jsx
+++ src/components/carousel/HanketsuCarousel.jsx
@@ -76,7 +76,7 @@ export default function HanketsuCarousel({ onCardClick }) {
         infinite
         disableButtonsControls={false}
         autoPlay={true}
-        autoPlayInterval={4000}
+        autoPlayInterval={2000}
         autoPlayStrategy="default"
         animationDuration={800}
         disableDotsControls={false}
